34753d5553e11364227731ad74898e3c65e0619e
luizgregorio <luizgregoriogrg@gmail.com>
2025-08-22 17:31:01 -0400

    Create exercicio:fritar_ovo.py

---
+++ exercicio:fritar_ovo.py
@@ -0,0 +1,15 @@
+Exercicio 1: Linguagem de Progamação Python - Bradesco
+Fritar um ovo em no minimo 15 passos
+Tentativa Inicial:
+1. Pegue uma frigideira - 2.Pegue o Saleiro - 3. Pegue o Oleo
+4. Acenda o fogo - 5. Coloque o oleo na frigideira - 6. 
+7.Pegue o ovo - 8. Quebre o ovo - 9. Coloque o ovo na frigideira
+10. Espere o ovo fritar - 11. Retire o ovo da frigideira - 12. Coloque o Ovo frito no prato
+13. Adicione sal a gosto - 14. Sirva o ovo - 15. Lave a frigideira
+
+Resposta do Exercicio:
+1.Pegue um ovo - 2. Pegue uma frigideira - 3. Pegue o óleo
+4. Pegue o saleiro - 5. Coloque o óleo na frigideira - 6. Acenda o fogo
+7. Aqueça a frigideira com o óleo - 8. Quebre o ovo - 9. Coloque o ovo na frigideira
+10. Adicione sal a gosto - 11. Espere o ovo fritar - 12. Retire o ovo da frigideira
+13. Coloque o ovo frito no prato - 14. Sirva o ovo - 15. Lave a frigideira
