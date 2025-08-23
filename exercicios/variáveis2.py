68492132693eb5d3fa9d69df8c614ef0b23a028a
luizgregorio <luizgregoriogrg@gmail.com>
2025-08-23 08:31:21 -0400

    Update variáveis2.py

--- exercicios/variáveis2.py
+++ exercicios/variáveis2.py
@@ -2,9 +2,20 @@ Exercicio 3: Linguagem de progamação Python - Bradesco
 Considere três variáveis A 5, B 8 e C 1. Quais seriam os resultados das expressões a seguir?
 Assinale Verdadeiro para as expressões corretas e Falso para as incorretas.
 
+Tentativa inicial:
 A = B and B > C    F
-A < > B or B < C   F
-A > B not -----    V
+A < > B or B < C   V
+A > B not -----    F
 A < B and B > C    V
 A >= B or B = C    F
 A <= B not -----   F
+
+Resposta do exercicio:
+A = B and B > C    F
+A < > B or B < C   F
+A > B not -----    F
+A < B and B > C    V
+A >= B or B = C    F
+A <= B not -----   F
+
+5 de 6 questões
