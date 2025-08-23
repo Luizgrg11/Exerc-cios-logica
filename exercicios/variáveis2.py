b6ffb8105c51c76236eb5848c4e6415106b2f4cc
luizgregorio <luizgregoriogrg@gmail.com>
2025-08-23 08:28:05 -0400

    Create variáveis2.py

---
+++ exercicios/variáveis2.py
@@ -0,0 +1,9 @@
+Considere três variáveis A 5, B 8 e C 1. Quais seriam os resultados das expressões a seguir?
+Assinale Verdadeiro para as expressões corretas e Falso para as incorretas.
+
+A = B and B > C    F
+A < > B or B < C   F
+A > B not -----    V
+A < B and B > C    V
+A >= B or B = C    F
+A <= B not -----   F
