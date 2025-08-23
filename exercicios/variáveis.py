439feeb5a11c6d0db0a91ecba43c763b7545f6b7
luizgregorio <luizgregoriogrg@gmail.com>
2025-08-23 08:20:08 -0400

    Create variáveis.py

---
+++ exercicios/variáveis.py
@@ -0,0 +1,12 @@
+Considere duas variáveis A 5 e B 3. Quais seriam os resultados das expressões a seguir?
+Assinale Verdadeiro para as expressões corretas e Falso para as incorretas.
+
+Tentativa Inicial
+A=B   F
+A<>B  V
+A>B   V
+A<B   F
+A>=B  V
+A<=B  F
+
+100% de acertos
